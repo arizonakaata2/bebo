38c19726a5199dcfa2706d211241a0a22fe298a0
arizonakaata2 <arizonakaata2@gmail.com>
2026-03-14 14:43:40 +1300

    Update page.tsx
    update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,21 @@
-export default function ProfilePage() {
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  const [likes, setLikes] = useState(0);
+
   return (
     <div style={{ padding: 40 }}>
-      <h1>Profile</h1>
-      <p>Your profile page.</p>
+      <h1>Bebo Feed</h1>
+
+      <div style={{border:"1px solid #333", padding:20, marginTop:20}}>
+        <h3>Arizona</h3>
+        <p>This is my first post!</p>
+
+        <button onClick={() => setLikes(likes + 1)}>
+          ❤️ Like ({likes})
+        </button>
+      </div>
     </div>
   );
 }
