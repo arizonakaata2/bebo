e34b87c6156036023863714886caf2302b3901b6
arizonakaata2 <arizonakaata2@gmail.com>
2026-03-14 13:03:10 +1300

    Create page.tsx

---
+++ app/app/profile/page.tsx
@@ -0,0 +1,15 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>Your Profile</h1>
+
+      <p>Username</p>
+      <p>Followers</p>
+      <p>Following</p>
+
+      <hr/>
+
+      <p>Your posts will appear here.</p>
+    </div>
+  )
+}
