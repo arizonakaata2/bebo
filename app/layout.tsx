04e15ac7e1d55c8f03b69d678e72a9ff7ba2c954
arizonakaata2 <arizonakaata2@gmail.com>
2026-03-14 14:50:30 +1300

    Update layout.tsx
    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,14 @@ export default function RootLayout({
   return (
     <html>
       <body style={{ background: "#000", color: "white", fontFamily: "Arial" }}>
+
+        <nav style={{ padding:20, borderBottom:"1px solid #333" }}>
+          <a href="/" style={{marginRight:20}}>Home</a>
+          <a href="/profile">Profile</a>
+        </nav>
+
         {children}
+
       </body>
     </html>
   );
